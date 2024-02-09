02288d30ed04dbf836940bb3f0d95909fed3fb7c
Revati Natu <124581682+Revati-N@users.noreply.github.com>
2024-02-09 19:59:09 +0530

    Day 6

---
+++ Learning/Day_6.r
@@ -0,0 +1,18 @@
+# Preprocessing Data
+
+?is.na
+example <- c("A",1,6,7,NA,"B")
+example
+
+mean(example)
+
+is.na(example) # Tells whether any of it is NA / Missing
+
+length(example)
+
+mean(example,na.rm = TRUE)
+
+median(example,na.rm = TRUE)
+
+1/0 # Infinity
+
